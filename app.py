ead1faaed22bf05970006aa2355c6078b4ac95e0
josephjdkim <josephjdkim@gmail.com>
2019-01-11 13:09:41 -0500

    initial setup of new db configuration (lol @ foreign keys)

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from api import app
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models import db
 
 app = Flask(__name__)
 
-db = SQLAlchemy()
-
 POSTGRES = {
-    'user': 'postgres',
-    'pw': 'password',
-    'db': 'my_database',
-    'host': 'localhost',
-    'port': '5432'
+    "user": "postgres",
+    "pw": "password",
+    "db": "my_database",
+    "host": "localhost",
+    "port": "5432"
 }
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://%(user)s:%(pw)s@%(host)s:\
+    %(port)s/%(db)s" % POSTGRES
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def main():
     return "main"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
